a06a8012df2816a7c8520f30e36e3505f81d919e
elTortuga <mds831@gmail.com>
2018-08-10 16:39:54 -0700

    Create bezier curve from n control points with text example

--- Assets/src/CreateBezier.cs
+++ Assets/src/CreateBezier.cs
@@ -4,49 +4,75 @@ using UnityEngine;
 
 public class CreateBezier : MonoBehaviour
 {
-
     public Vector3[] controlPoints;
-    public int accuracy;
     private Vector3[] bezierPoints;
+
+    public int accuracy;
 
     // Use this for initialization
     void Start()
     {
-
+        Debug.DrawLine(this.controlPoints[0], this.controlPoints[1], Color.red, 100000f, false);
+        Debug.DrawLine(this.controlPoints[2], this.controlPoints[1], Color.red, 100000f, false);
+        Debug.DrawLine(this.controlPoints[2], this.controlPoints[3], Color.red, 100000f, false);
+        Debug.DrawLine(this.controlPoints[4], this.controlPoints[3], Color.red, 100000f, false);
+        Testing();
     }
 
+    private Vector3[] CreateBezierCurve(Vector3[] controlPoints)
+    {
+        Vector3[][] setOfVectorSets = new Vector3[controlPoints.Length - 1][];
+        for (int i = 0; i < controlPoints.Length - 1; i++)
+        {
+            setOfVectorSets[i] = CreateVectorsFromControlPoints(controlPoints[i], controlPoints[i + 1]);
+        }
+        return CreateBezierVectors(setOfVectorSets);
+    }
 
-	private Vector3[] CreateBezierVectors(Vector3[][] setsOfVectors){
+    private Vector3[] CreateBezierVectors(Vector3[][] outerSetOfVectorSets)
+    {
+        if (outerSetOfVectorSets.Length == 2)
+        {
+            return CreateVectorsFromTwoSetsOfVectors(outerSetOfVectorSets[0], outerSetOfVectorSets[1]);
+        }
 
+        Vector3[][] innerSetOfVectorSets = new Vector3[outerSetOfVectorSets.Length - 1][];
 
+        for (int i = 0; i < outerSetOfVectorSets.Length - 1; i++)
+        {
+            innerSetOfVectorSets[i] = CreateVectorsFromTwoSetsOfVectors(outerSetOfVectorSets[i], outerSetOfVectorSets[i + 1]);
+        }
 
-		return new Vector3[0];
-	}
+        return CreateBezierVectors(innerSetOfVectorSets);
+    }
 
-	// Create a set of vectors with each index representing a step in an interpolated 
+    // Create a set of vectors with each index representing a step in a collection interpolated values.
     private Vector3[] CreateVectorsFromTwoSetsOfVectors(Vector3[] a, Vector3[] b)
     {
-		Vector3[] localVectors = new Vector3[this.accuracy];
-		for (int t = 0; t < this.accuracy; t++)
-		{
-			localVectors[t] = LerpVectorsAtT(a[t], b[t], t+1);
-		}
-		return localVectors;
+        Vector3[] localVectors = new Vector3[this.accuracy];
+        float t;
+        for (int i = 0; i < this.accuracy; i++)
+        {
+            t = (i + 1f) / this.accuracy;
+            localVectors[i] = LerpVectorsAtT(a[i], b[i], t);
+        }
+        return localVectors;
     }
 
     private Vector3[] CreateVectorsFromControlPoints(Vector3 a, Vector3 b)
     {
-		int numberOfSteps = accuracy;
-		Vector3[] localVectors = new Vector3[this.accuracy]; 
-		for (int t = 0; t < this.accuracy; t++)
-		{
-			localVectors[t] = LerpVectorsAtT(a, b, this.accuracy/(t+1));
-		}
-		return localVectors;
+        Vector3[] localVectors = new Vector3[this.accuracy];
+        float t;
+        for (int i = 0; i < this.accuracy; i++)
+        {
+            t = (i + 1f) / this.accuracy;
+            localVectors[i] = LerpVectorsAtT(a, b, t);
+        }
+        return localVectors;
     }
 
     //Interpolate two vectors 
-    private Vector3 LerpVectorsAtT(Vector3 a, Vector3 b, int t)
+    private Vector3 LerpVectorsAtT(Vector3 a, Vector3 b, float t)
     {
         return new Vector3
         (
@@ -56,6 +82,16 @@ public class CreateBezier : MonoBehaviour
         );
     }
 
+    private void Testing()
+    {
+        DrawDebugLineForVectors(CreateBezierCurve(this.controlPoints));
+    }
 
-
+    private void DrawDebugLineForVectors(Vector3[] vectors)
+    {
+        for (int i = 0; i < vectors.Length - 1; i++)
+        {
+            Debug.DrawLine(vectors[i], vectors[i + 1], Color.red, 100000, true);
+        }
+    }
 }
